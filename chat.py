4afaf2fea72fb04e7f1c03e6b41c119f76c012ad
Paulo Freitas <65922682+Ammonchip@users.noreply.github.com>
2024-02-01 15:29:13 -0400

    chatbot addition
    chatbot for plant control, need connection via opcua be implemented

---
+++ chat.py
@@ -0,0 +1,25 @@
+import logging
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('6586024100:AAFz11O_r088xe52z0M1Nqq501Mn0YoZpuU').build()
+    
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    
+    application.run_polling()
